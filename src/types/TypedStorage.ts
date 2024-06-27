58a9059d2d8c146434f65856e245c54774a82b91
jill64 <contact@jill64.dev>
2024-06-27 20:51:44 +0900

    feat: addListener

--- src/types/TypedStorage.ts
+++ src/types/TypedStorage.ts
@@ -1,5 +1,7 @@
 export type TypedStorage<T> = {
   get: () => T
   set: (value: T) => void
+  /** @deprecated Use addListener instead */
   subscribe: (callback: (value: T) => unknown) => () => void
+  addListener: (callback: (value: T) => unknown) => void
 }
